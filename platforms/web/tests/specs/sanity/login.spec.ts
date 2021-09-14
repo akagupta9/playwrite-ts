2e8b892ea5d344099db551fd1278199efc563e8c
Akanskha Gupta <akankshag@vedantu.com>
2021-09-14 19:25:46 +0530

    fix(login): minor changes

--- platforms/web/tests/specs/sanity/login.spec.ts
+++ platforms/web/tests/specs/sanity/login.spec.ts
@@ -1,19 +1,21 @@
-import { expect, test } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
 import { HomePage } from "../../pages/home.page";
 
 const proxymise = require("proxymise");
 
 test.describe("HOME : UI Verification", () => {
-  test.beforeEach(async ({ page }) => {
+  let page: Page;
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
     await page.goto("https://www.linkedin.com/");
   });
 
-  test("should verify the Title", async ({ page }) => {
+  test("should verify the Title", async () => {
     const title = await page.title();
     await expect(title).toContain("LinkedIn");
   });
 
-  test("should verify presence of Home Tab", async ({ page }) => {
+  test("should verify presence of Home Tab", async () => {
     const isHomeTabPresent = await (
       await page.locator("//span[text()='Home']")
     ).isVisible({ timeout: 5000 });
@@ -21,7 +23,7 @@ test.describe("HOME : UI Verification", () => {
   });
 
   // Async Chaining
-  test("should check the chaining of methods", async ({ page }) => {
+  test("should check the chaining of methods", async () => {
     const title = await proxymise(HomePage)
       .init(page)
       .navigateTo("https://www.linkedin.com/")
